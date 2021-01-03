4ba08d08f363965e98ee7664f7f76b95dc4047d9
Carl Edwin Antonio Nascimento <carlinstr@gmail.com>
2021-01-03 17:25:06 -0300

    MethodNotations para mostrar os tipos de métodos e chamadas. Diferença entre objects e class.

---
+++ src/lectures/part1basics/Objects.scala
@@ -0,0 +1,77 @@
+package lectures.part1basics
+
+object Objects {
+
+  //SCALA DOES NOT HAVE CLASS-LEVEL FUNCTIONALITY ("static)
+  //Scala 'object' in a SINGLETON instance
+  //Scala object = SINGLETON INSTANCE
+
+  //Scala Applications = Scala object with have to be a main method
+  //def main(args: Array[String]): Unit = {}
+  //OR extends App
+  def main(args: Array[String]): Unit = {
+
+    println(Person.N_EYES)
+    println(Person.canFly)
+
+    val mary = Person
+    val jon = Person
+
+    println(mary == jon)
+    println(s"Mary and Jon have the same instance? -> ${mary == jon}")
+
+    val ana = new Person
+    val paul = new Person
+    println(ana == paul)
+    println(s"Ana and Paul have the same instance? -> ${ana == paul}")
+
+    val personObject1 = Person
+    val personObject2 = Person
+    println(s"PersonObject1 and PersonObject2 have the same instance? -> ${personObject1 == personObject2}")
+
+    val personInstance1 = new Person
+    println(s"PersonObject1 and PersonInstance1 have the same instance? -> ${personObject1 == personInstance1}")
+
+    val personInstance2 = new Person
+    println(s"PersonInstance1 and PersonInstance2 have the same instance? -> ${personInstance1 == personInstance2}")
+
+    val katy = new Person("Katy")
+    val daniel = new Person("Daniel")
+    println(s"${katy.name} and ${daniel.name} have the same instance? -> ${katy == daniel}")
+
+    val jerry = new Person("Jerry")
+    val jullian = new Person("Jullian")
+    val pierry = new Person("Pierry")
+    val mandum = new Person("Mandum")
+
+    val bobbie = Person.apply(katy, daniel)
+    val bobbie2 = Person(jerry, jullian)
+    val bobbie3 = (pierry, mandum)
+  }
+
+
+  //COMPANIONS object and class in the same class
+  object Person{ //type + its only instance
+    //"static"/"class" - level functionality
+    val N_EYES = 2
+    def canFly: Boolean = false
+
+    //factory method
+    def apply(mother: Person, father: Person): Person = new Person("Bobbie")
+  }
+
+  class Person(val name: String){
+    //instance-level functionality
+    def this() = this("")
+  }
+
+
+
+
+
+
+
+
+
+
+}
--- src/lectures/part2oop/MethodNotations.scala
+++ src/lectures/part2oop/MethodNotations.scala
@@ -2,4 +2,79 @@ package lectures.part2oop
 
 object MethodNotations extends App {
 
+  class Person(val name: String, favoriteMovie: String, val age: Int = 0){
+    def this(name: String) = this(name, "", 0)
+    def this(name: String, favoriteMovie: String) = this(name, favoriteMovie, 0)
+    def likes(movie: String): Boolean = movie == favoriteMovie
+    def hangOutWith(person: Person): String = s"${this.name} is hanging out with ${person.name}"
+    def +(person: Person): String = s"${this.name} is hanging out with ${person.name}"
+    def +(nickname: String): Person = new Person(s"$name ($nickname)", favoriteMovie)
+    def unary_~ : String = s"${this.name} with the age incrementer to ${this.age + 1}"
+    def unary_+ : Person = new Person(name, favoriteMovie, age + 1)
+    def unary_! : String = s"$name, what the heck?!"
+    def ~ : String = "Hello !!"
+    def unary_- : String = "Hello !!"
+    def isAlive: Boolean = true
+    def apply(): String = s"Hi, my name's $name and I like $favoriteMovie"
+    def apply(times: Int) = s"${this.name} watched Inception $times times"
+    def learns : String = s"${this.name} learns Scala"
+    def learns(thing: String) : String = s"${this.name} learns $thing"
+  }
+
+  val mary = new Person("Mary", "Inception")
+  println(mary.likes("Inception"))
+  println(mary.likes("Inceptionn"))
+  println(mary likes "Inceptionn") //equivalent
+
+  //infix notation = operator notation(syntactic sugar)
+  //"operators" in Scala
+  val tom = new Person("Tom", "Fight Club")
+  println(mary hangOutWith tom)
+  println(mary + tom)
+  println(mary .+(tom))
+
+  println(1 + 2)
+  println(1.+(2))
+  //ALL OPERATORS ARE METHODS
+  //Akka actors have ! ?
+
+  //prefix notation
+  val x = -1 // equivalent with 1.unary_-
+  val y = 1.unary_-
+  //unary_prefix only works with - + ~ !
+
+  println(!mary) //prefix
+  println(mary.unary_!)
+  //println(~mary)//isn't works, needs to be declared with unary_- to use how prefix or sulfix
+  println(-mary)
+  println(mary unary_-)
+  println(mary.unary_-)
+
+  //postfix notation
+  println(mary.isAlive)
+  println(mary isAlive)
+
+  //apply - the () and type of return are very important at the method declaration
+  println(mary.apply())
+  println(mary.apply)
+  println(mary apply)
+  println(mary()) //equivalent
+
+  //exercises
+
+
+  val maryE = new Person("Mary", "Inception")
+  println(maryE + "the rockstar")
+  println((maryE + "the rockstar")())
+  println((maryE + "the rockstar").apply())
+
+  val maryAge = new Person("Mary")
+  println(~maryAge)
+  println(+maryAge.age)
+  println((+maryAge).age)
+  println(maryAge learns)
+  println(maryAge learns("Hadoop"))
+  println(maryAge(10))
+  println(maryAge.apply(2))
 }
+
