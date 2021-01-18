ae6a2a77fe7182e97635830cd61bac9d09394d56
Carl Edwin Antonio Nascimento <carlinstr@gmail.com>
2021-01-17 21:50:48 -0300

    Tuples and Maps

--- src/lectures/part3fp/TuplesAndMaps.scala
+++ src/lectures/part3fp/TuplesAndMaps.scala
@@ -117,6 +117,8 @@ object TuplesAndMaps extends App {
   /*
 
   1. what would happen if I had two original entries "Jim -> 555 and "JIM" -> 900?
+  !!!! careful with mapping keys
+
   2. Overly simplified network based on maps
     Person = String
       - add a person to the network
@@ -130,5 +132,99 @@ object TuplesAndMaps extends App {
       - if there is a social connection between two people (direct or not)
    */
 
+  val phonebook2 = Map(("Jim", 600),("JIM", 3335))
+  println(phonebook2)
+  println(phonebook2.map(x => x._1.toLowerCase()))
+
+  def add(network: Map[String, Set[String]], person: String): Map[String, Set[String]] =
+    network + (person -> Set())
+
+    def friend(network: Map[String, Set[String]], a: String, b: String): Map[String, Set[String]] = {
+      val friendsA = network(a)
+      val friendsB = network(b)
+      network + (a -> (friendsA + b)) + (b -> (friendsB + a))
+    }
+
+    def unfriend(network: Map[String, Set[String]], a: String, b: String): Map[String, Set[String]] = {
+      val friendsA = network(a)
+      val friendsB = network(b)
+      network + (a -> (friendsA - b)) + (b -> (friendsB - a))
+    }
+
+    def remove(network: Map[String, Set[String]], person: String): Map[String, Set[String]] = {
+      def removeAux(friends: Set[String], networkAcc: Map[String, Set[String]]): Map[String, Set[String]] =
+        if(friends.isEmpty) networkAcc
+        else removeAux(friends.tail, unfriend(networkAcc, person, friends.head))
+
+        val unfriended = removeAux(network(person), network)
+      unfriended - person
+    }
+
+    val empty: Map[String, Set[String]] = Map()
+    val network = add(add(empty, "Bob"), "Mary")
+
+  println(network)
+
+  println(friend(network, "Bob", "Mary"))
+
+  println(unfriend(friend(network, "Bob", "Mary"), "Bob", "Mary"))
+
+  println(remove(friend(network, "Bob", "Mary"), "Bob"))
+
+  //Jim, Bob, Mary
+  val people = add(add(add(add(empty, "Bob"), "Mary"), "Jim"),"Kevin")
+  println(people)
+
+  val jimBob = friend(people, "Bob", "Jim")
+  println(jimBob)
+
+  val maryBob = friend(jimBob, "Bob", "Mary")
+  println(maryBob)
+
+  def numberOfFriends(network: Map[String, Set[String]], person: String): Int =
+    if(!network.contains(person)) 0
+    else network(person).size
+
+  println(numberOfFriends(maryBob, "Bob"))
+  println(numberOfFriends(maryBob, "Mary"))
+  println(numberOfFriends(maryBob, "Jim"))
+
+  def mostFriends(network: Map[String, Set[String]]): String =
+    network.maxBy(pair => pair._2.size)._1
+
+  println(mostFriends(network))
+
+  def nPeopleWithNoFriendsFilter(network: Map[String, Set[String]]): Int =
+    network.filterKeys(k => network(k).isEmpty).size
+
+  println(nPeopleWithNoFriendsFilter(maryBob))
+
+  def nPeopleWithNoFriendsCount(network: Map[String, Set[String]]): Int =
+    network.count(pair => pair._2.isEmpty)
+
+  println(nPeopleWithNoFriendsCount(maryBob))
+
+  def nPeopleWithNoFriendsCountSxSugar(network: Map[String, Set[String]]): Int =
+    network.count(_._2.isEmpty)
+
+  println(nPeopleWithNoFriendsCountSxSugar(maryBob))
+
+  def socialConnection(network: Map[String, Set[String]], a: String, b: String): Boolean = {
+    def bfs(target: String, consideredPeople: Set[String], discoveredPeople: Set[String]): Boolean = {
+      if(discoveredPeople.isEmpty) false
+      else{
+        val person = discoveredPeople.head
+        if(person == target) true
+        else if (consideredPeople.contains(person)) bfs(target, consideredPeople, discoveredPeople.tail)
+        else bfs(target, consideredPeople + person, discoveredPeople.tail ++ network(person)) //++ append
+      }
+    }
+
+    bfs(b, Set(), network(a) + a)
+  }
+
+  println(socialConnection(maryBob, "Mary", "Jim"))
+  println(socialConnection(maryBob, "Mary", "Bob"))
+  println(socialConnection(maryBob, "Mary", "Kevin"))
 
 }
